c99f1158de1dfb9857398a05634e4705b44ce193
Kim DoHyeon <dohyun51731@gmail.com>
2023-12-26 23:10:45 +0900

    변환 결과 저장 기능 생성 중

--- lib/length.dart
+++ lib/length.dart
@@ -13,12 +13,12 @@ class _LengthConverterState extends State<LengthConverter> {
   String result = "";
   String fromUnit = "mm";
   String toUnit = "mm";
+  List<String> transResult = [];
 
   final List<String> units = ["mm", "cm", "m", "km", "in", "ft", "yd", "mile"];
 
   @override
   void dispose() {
-    // Clean up the controller when the widget is disposed
     lengthController.dispose();
     super.dispose();
   }
@@ -29,116 +29,127 @@ class _LengthConverterState extends State<LengthConverter> {
       appBar: AppBar(
         title: const Text('길이 변환'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            TextField(
-              controller: lengthController,
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
-                setState(() {
-                  length = double.tryParse(value) ?? 0;
-                });
-              },
-              decoration: const InputDecoration(
-                labelText: '길이 입력',
-                hintText: '길이를 입력하세요',
-              ),
-              style: const TextStyle(fontSize: 20),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-            ),
-            Row(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Expanded(
-                  child: DropdownButton<String>(
-                    value: fromUnit,
-                    onChanged: (value) {
-                      setState(() {
-                        fromUnit = value!;
-                      });
-                    },
-                    items: units.map<DropdownMenuItem<String>>((String unit) {
-                      return DropdownMenuItem<String>(
-                        value: unit,
-                        child: Text(
-                          unit,
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                      );
-                    }).toList(),
+                TextField(
+                  controller: lengthController,
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    setState(() {
+                      length = double.tryParse(value) ?? 0;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                    labelText: '길이 입력',
+                    hintText: '길이를 입력하세요',
                   ),
+                  style: const TextStyle(fontSize: 20),
                 ),
                 const Padding(
                   padding: EdgeInsets.all(8.0),
                 ),
-                Expanded(
-                  child: DropdownButton<String>(
-                    value: toUnit,
-                    onChanged: (value) {
-                      setState(() {
-                        toUnit = value!;
-                      });
+                Row(
+                  children: [
+                    Expanded(
+                      child: DropdownButton<String>(
+                        value: fromUnit,
+                        onChanged: (value) {
+                          setState(() {
+                            fromUnit = value!;
+                          });
+                        },
+                        items: units.map<DropdownMenuItem<String>>((String unit) {
+                          return DropdownMenuItem<String>(
+                            value: unit,
+                            child: Text(
+                              unit,
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                    ),
+                    Expanded(
+                      child: DropdownButton<String>(
+                        value: toUnit,
+                        onChanged: (value) {
+                          setState(() {
+                            toUnit = value!;
+                          });
+                        },
+                        items: units.map<DropdownMenuItem<String>>((String unit) {
+                          return DropdownMenuItem<String>(
+                            value: unit,
+                            child: Text(
+                              unit,
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    convertLength();
+                  },
+                  child: const Text('변환', style: TextStyle(fontSize: 16)),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                ),
+                Text(
+                  '결과: $result',
+                  style: const TextStyle(fontSize: 16),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      resetValues();
                     },
-                    items: units.map<DropdownMenuItem<String>>((String unit) {
-                      return DropdownMenuItem<String>(
-                        value: unit,
-                        child: Text(
-                          unit,
-                          style: const TextStyle(fontSize: 20),
-                        ),
-                      );
-                    }).toList(),
+                    child: const Text('초기화', style: TextStyle(fontSize: 16)),
                   ),
                 ),
               ],
             ),
-            const Padding(
-              padding: EdgeInsets.all(16.0),
+          ),
+          Expanded(
+            child: Image.asset(
+              'assets/body_back/ladder.png',
+              fit: BoxFit.contain,
             ),
-            ElevatedButton(
-              onPressed: () {
-                convertLength();
-              },
-              child: const Text('변환', style: TextStyle(fontSize: 16)),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-            ),
-            Text(
-              '결과: $result',
-              style: const TextStyle(fontSize: 16),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: ElevatedButton(
-                onPressed: () {
-                  resetValues();
-                },
-                child: const Text('초기화', style: TextStyle(fontSize: 16)),
-              ),
-            ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
 
   void convertLength() {
     double resultValue = length;
-    // Convert to meters first
+    // 미터로 변환
     resultValue = convertToMeter(resultValue, fromUnit);
-    // Convert from meters to the target unit
+    // 다른 단위로 변환
     resultValue = convertFromMeter(resultValue, toUnit);
 
     setState(() {
       result = '$resultValue $toUnit';
+      recordResult(result);
     });
   }
 
@@ -150,52 +161,57 @@ class _LengthConverterState extends State<LengthConverter> {
       result = "";
       fromUnit = "mm";
       toUnit = "mm";
+      transResult.clear();
     });
   }
 
-  double convertToMeter(double value, String unit) {
-    switch (unit) {
-      case "mm":
-        return value / 1000;
-      case "cm":
-        return value / 100;
-      case "m":
-        return value;
-      case "km":
-        return value * 1000;
-      case "in":
-        return value * 0.0254;
-      case "ft":
-        return value * 0.3048;
-      case "yd":
-        return value * 0.9144;
-      case "mile":
-        return value * 1609.34;
-      default:
-        return value;
-    }
+  void recordResult(String result) {
+    transResult.insert(0, '변환 결과: $result');
   }
+}
 
-  double convertFromMeter(double value, String unit) {
-    switch (unit) {
-      case "mm":
-        return value * 1000;
-      case "cm":
-        return value * 100;
-      case "m":
-        return value;
-      case "km":
-        return value / 1000;
-      case "in":
-        return value / 0.0254;
-      case "ft":
-        return value / 0.3048;
-      case "yd":
-        return value / 0.9144;
-      case "mile":
-        return value / 1609.34;
-      default:
-        return value;
-    }
+double convertToMeter(double value, String unit) {
+  switch (unit) {
+    case "mm":
+      return value / 1000;
+    case "cm":
+      return value / 100;
+    case "m":
+      return value;
+    case "km":
+      return value * 1000;
+    case "in":
+      return value * 0.0254;
+    case "ft":
+      return value * 0.3048;
+    case "yd":
+      return value * 0.9144;
+    case "mile":
+      return value * 1609.34;
+    default:
+      return value;
+  }
+}
+
+double convertFromMeter(double value, String unit) {
+  switch (unit) {
+    case "mm":
+      return value * 1000;
+    case "cm":
+      return value * 100;
+    case "m":
+      return value;
+    case "km":
+      return value / 1000;
+    case "in":
+      return value / 0.0254;
+    case "ft":
+      return value / 0.3048;
+    case "yd":
+      return value / 0.9144;
+    case "mile":
+      return value / 1609.34;
+    default:
+      return value;
   }
 }
